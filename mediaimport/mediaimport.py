39554413103cdecac206d28cfae077332e71dc71
Houssam Salem <houssam.salem.au@gmail.com>
2015-03-03 15:27:15 +1100

    Version bump (2.1)

--- mediaimport/mediaimport.py
+++ mediaimport/mediaimport.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Version: 2.0
+# Version: 2.1
 #
 # This is an Anki add-on for creating notes by importing media files from a
 # user-selected directory. The user is able to map properties of the imported
